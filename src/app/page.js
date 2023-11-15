4e57275b1233973a9c0808ea5c406e230bf4d937
Duh0127 <rodatupmocgamer22@gmail.com>
2023-11-14 23:41:03 -0300

    criando homepage do projeto

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,33 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+
   return (
     <main className={styles.main}>
-      
+      <section className={styles.first_section}>
+        <div>
+          <h1 className={styles.first_title}>Seja nosso cliente</h1>
+          <p className={styles.first_description}>
+            Mude seu estilo de vida e tenha
+            mais saúde e disposição no seu
+            dia-a-dia com a ajuda de
+            nosso sistema de criação de
+            treinos e dietas inteligênte.
+          </p>
+        </div>
+      </section>
+
+      <section className={styles.second_section}>
+        <Image className={styles.healthy_food} src="/healthy_food.jpg" width={450} height={650} alt='Comida Saudável' />
+        
+        <div className={styles.information}>
+          <h2 className={styles.second_title}>Dietas</h2>
+          <p className={styles.second_description}>
+            Crie dietas personalizadas de acordo com
+            seus objetivos e necessidades.
+          </p>
+        </div>
+      </section>
     </main>
   )
 }
